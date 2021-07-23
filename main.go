5f92fca9d35271bcf6539ee94e1c569bbadf879f
opusvar <sprojdev@gmail.com>
2021-07-23 08:39:13 -0400

    Variables and print statement completed

--- main.go
+++ main.go
@@ -1,3 +1,11 @@
 package main
 
 import f "fmt"
+
+func main() {
+	var publisher, writer, artist, title string
+	var year, pageNumber uint
+	var grade float32
+	f.Println(title, "written by", writer, "drawn by", artist)
+
+}
