2bb5dc134cebb5d90ba0e2135306c1d21e02395a
opusvar <sprojdev@gmail.com>
2021-07-23 08:56:57 -0400

    Refactored print statement, added second comicbook entry

--- main.go
+++ main.go
@@ -13,6 +13,13 @@ func main() {
 	year = 1997
 	pageNumber = 14
 	grade = 6.5
-	f.Println(title, "written by", writer, "drawn by", artist)
-
+	f.Println(title, "published by", publisher, "year", year, "written by", writer, "drawn by", artist, "pages", pageNumber, "Grade", grade)
+	// next comic book, publishers are the same
+	title = "Epic Vol. 1"
+    writer = "Ryan N. Shaw"
+	artist = "Phobe Paperclips"
+	year = 2013
+	pageNumber = 160
+	grade = 9.0
+	f.Println(title, "published by", publisher, "year", year, "written by", writer, "drawn by", artist, "pages", pageNumber, "Grade", grade)
 }
