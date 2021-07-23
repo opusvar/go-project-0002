71aa78aae2c83e096870ee552b0d07bf877455c7
opusvar <sprojdev@gmail.com>
2021-07-23 08:42:34 -0400

    First comicbook added

--- main.go
+++ main.go
@@ -6,6 +6,13 @@ func main() {
 	var publisher, writer, artist, title string
 	var year, pageNumber uint
 	var grade float32
+	title = "Mr. GoToSleep"
+	writer = "Tracy Hatchet"
+	artist = "Jewel Tampson"  
+	publisher = "DizzyBooks Publishing Inc."
+	year = 1997
+	pageNumber = 14
+	grade = 6.5
 	f.Println(title, "written by", writer, "drawn by", artist)
 
 }
